d9707160f11e5ef4520257c6ca3688ddc9a79d68
Simon Otter <simon.otter@ilpvfx.com>
2020-02-25 15:39:47 +0100

    Allow the WSGI runner to be set to either gunicorn or werkzeug.

--- source/interstate_love_song/__main__.py
+++ source/interstate_love_song/__main__.py
@@ -1,33 +1,61 @@
-from gunicorn.app.base import BaseApplication
+import argparse
+import logging
+
 from .http import get_falcon_api, BrokerResource
 
 
-class _GunicornApp(BaseApplication):
-    def __init__(self, app, options=None):
-        self.app = app
-        self.options = options or {}
-        super().__init__()
+def gunicorn_runner(wsgi, host, port):
+    from gunicorn.app.base import BaseApplication
 
-    def init(self, parser, opts, args):
-        pass
+    class _GunicornApp(BaseApplication):
+        def __init__(self, app, options=None):
+            self.app = app
+            self.options = options or {}
+            super().__init__()
 
-    def load(self):
-        return self.app
+        def init(self, parser, opts, args):
+            pass
 
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
+        def load(self):
+            return self.app
+
+        def load_config(self):
+            config = {
+                key: value
+                for key, value in self.options.items()
+                if key in self.cfg.settings and value is not None
+            }
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+    options = {
+        "bind": "{}:{}".format(host, port),
+        "certfile": "selfsign.crt",
+        "keyfile": "selfsign.key",
+        "log-level": "debug",
+    }
+    _GunicornApp(wsgi, options).run()
+
+
+def werkzeug_runner(wsgi, host, port):
+    from werkzeug.serving import run_simple
+
+    run_simple(host, port, wsgi, ssl_context=("selfsign.crt", "selfsign.key"))
 
 
 if __name__ == "__main__":
-    options = {
-        "bind": "localhost:60444",
-        "certfile": "selfsign.crt",
-        "keyfile": "selfsign.key",
-    }
-    _GunicornApp(get_falcon_api(BrokerResource()), options).run()
+    logging.basicConfig()
+
+    argparser = argparse.ArgumentParser("interstate_love_song")
+    argparser.add_argument("-s", "--server", choices=["werkzeug", "gunicorn"], default="gunicorn")
+    argparser.add_argument("--host", default="localhost")
+    argparser.add_argument("-p", "--port", default=60444, type=int)
+
+    args = argparser.parse_args()
+
+    wsgi = get_falcon_api(BrokerResource())
+
+    if args.server == "werkzeug":
+        werkzeug_runner(wsgi, args.host, args.port)
+    elif args.server == "gunicorn":
+        gunicorn_runner(wsgi, args.host, args.port)
