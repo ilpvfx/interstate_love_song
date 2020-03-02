1dd76b2a747ecf20fd73f0f6320a06efc93a13ab
Simon Otter <simon.otter@ilpvfx.com>
2020-03-02 15:53:35 +0100

    Delete the old PoC broker.

--- source/interstate_love_song/old/__init__.py
+++
@@ -1,4 +0,0 @@
-# :coding: utf-8
-# :copyright: Copyright (c) 2019 ilp
-
-from ._version import __version__
--- source/interstate_love_song/old/_version.py
+++
@@ -1,4 +0,0 @@
-# :coding: utf-8
-# :copyright: Copyright (c) 2019 ilp
-
-__version__ = "0.0.1"
